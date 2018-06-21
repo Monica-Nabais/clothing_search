078d7a5b38de6a48c80b3d1f0693dff5c4dfdddc
Monica Nabais <monicamnabais@gmail.com>
2018-06-21 21:57:28 +0100

    parsing from bershka-dresses

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,16 @@
+require 'open-uri'
+require 'nokogiri'
 class ArticlesController < ApplicationController
+  def index
+    @images = []
+    file_bershka = 'bershka.html'
+    #file_zara = 'zara.html'
+    file_pull = 'pull.html'
+    doc_bershka = Nokogiri::HTML(File.open(file_bershka), nil, 'utf-8')
+    #doc_zara = Nokogiri::HTML(open('https://www.zara.com/pt/pt/mulher-vestidos-l1066.html'))
+    doc_pull = Nokogiri::HTML(File.open(file_pull), nil, 'utf-8')
+    #@images << doc_bershka.search('a img').map{ |i| i['src'] }
+    #@images << doc_zara.search('.product-grid-xmedia img').map{ |i| i['src'] }
+    @images << doc_pull.search('img').map{ |i| i['src'] }
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_21_125745) do
+ActiveRecord::Schema.define(version: 2018_06_21_130430) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.string "color"
+    t.string "article_type"
+    t.float "price"
+    t.string "size"
+    t.text "composition"
+    t.bigint "brand_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["brand_id"], name: "index_articles_on_brand_id"
+  end
+
+  create_table "brands", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "website"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -32,4 +58,5 @@ ActiveRecord::Schema.define(version: 2018_06_21_125745) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "articles", "brands"
 end
